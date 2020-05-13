c30d0a7c150b81f0460a065561f257de881f8846
Jimmy Emery <jemery@allcampus.com>
2020-05-12 20:15:10 -0400

    updating queries

--- src/schema/card/cardQueries.ts
+++ src/schema/card/cardQueries.ts
@@ -1,6 +1,11 @@
 export default {
   userCards: async (parent, args, { passport, prisma }, info) => {
     try {
+      console.log('---------------');
+      console.log('---------------');
+      console.log(passport.getUser());
+      console.log('---------------');
+      console.log('---------------');
       const userId = passport.getUserId();
 
       const finalArgs = {
