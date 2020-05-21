d80d0e430d28c7701192067f92e2a2fa6fa4ee6a
Jimmy Emery <jemery@allcampus.com>
2020-05-21 14:49:08 -0400

    updating card subscriptions

--- src/schema/card/cardSubscriptions.ts
+++ src/schema/card/cardSubscriptions.ts
@@ -1,22 +1,11 @@
 export default {
   count: {
     subscribe: (parent, args, { pubsub }, info) => {
-      // let count = 0;
-      //
-      // setInterval(() => {
-      //   // eslint-disable-next-line no-plusplus
-      //   count++;
-      //   pubsub.publish('count', {
-      //     count,
-      //   });
-      // }, 1000);
+      setInterval(() => {
+        pubsub.publish('count', { count: 1 });
+      }, 1000);
 
       return pubsub.asyncIterator('count');
-    },
-  },
-  userCards: {
-    subscribe: (parent, args, { pubsub }, info) => {
-      return pubsub.asyncIterator('userCards');
     },
   },
 };
