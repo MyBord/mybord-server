08758d3a51f045e7300d1ab39c1b2e3b3914a0b2
Jimmy Emery <jemery@allcampus.com>
2020-05-21 16:33:00 -0400

    updating sub

--- src/schema/card/cardSubscriptions.ts
+++ src/schema/card/cardSubscriptions.ts
@@ -1,16 +1,5 @@
 export default {
-  count: { // remove
-    subscribe: (parent, args, { pubsub }, info) => {
-      setInterval(() => {
-        // pubsub.publish('count', { count: 1 });
-      }, 1000);
-
-      return pubsub.asyncIterator('count');
-    },
-  },
   userCard: {
-    subscribe: (parent, args, { pubsub }, info) => {
-      return pubsub.asyncIterator('userCard'); // remove return statement
-    },
+    subscribe: (parent, args, { pubsub }, info) => pubsub.asyncIterator('userCard'),
   },
 };
