7afa49e14acc42667ccd96967964d06dd900f8c9
Jimmy <jimmy.e@outlook.com>
2020-06-21 14:56:21 -0400

    Is favorite is todo (#6)
    * updating schema
    
    * adding todo mutation

--- src/schema/card/cardMutations.ts
+++ src/schema/card/cardMutations.ts
@@ -72,4 +72,74 @@ export default {
       status: 403,
     });
   },
+  toggleFavoriteUserCard: async (parent, args, { passport, prisma }, info) => {
+    const userId = passport.getUserId();
+    const { cardId } = args.data;
+
+    const queryArgs = {
+      where: {
+        id: cardId,
+        user: {
+          id: userId,
+        },
+      },
+    };
+
+    // Make sure that the card that is trying to be deleted belongs to the user
+    const userCard = await prisma.query.cards(queryArgs, '{ id isFavorite }');
+
+    if (userCard.length > 0) {
+      const updateArgs = {
+        data: {
+          isFavorite: !userCard[0].isFavorite,
+        },
+        where: {
+          id: cardId,
+        },
+      };
+
+      const updatedCard = await prisma.mutation.updateCard(updateArgs, info);
+      return updatedCard;
+    }
+
+    throw new ServerError({
+      message: 'The user does not have access to delete this card',
+      status: 403,
+    });
+  },
+  toggleToDoUserCard: async (parent, args, { passport, prisma }, info) => {
+    const userId = passport.getUserId();
+    const { cardId } = args.data;
+
+    const queryArgs = {
+      where: {
+        id: cardId,
+        user: {
+          id: userId,
+        },
+      },
+    };
+
+    // Make sure that the card that is trying to be deleted belongs to the user
+    const userCard = await prisma.query.cards(queryArgs, '{ id isToDo }');
+
+    if (userCard.length > 0) {
+      const updateArgs = {
+        data: {
+          isToDo: !userCard[0].isToDo,
+        },
+        where: {
+          id: cardId,
+        },
+      };
+
+      const updatedCard = await prisma.mutation.updateCard(updateArgs, info);
+      return updatedCard;
+    }
+
+    throw new ServerError({
+      message: 'The user does not have access to delete this card',
+      status: 403,
+    });
+  },
 };
