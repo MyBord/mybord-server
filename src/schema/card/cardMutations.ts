a54a68873e6a06fd898c176b0038c715f4389e23
Jimmy Emery <jemery@allcampus.com>
2020-06-21 14:55:43 -0400

    adding todo mutation

--- src/schema/card/cardMutations.ts
+++ src/schema/card/cardMutations.ts
@@ -107,4 +107,39 @@ export default {
       status: 403,
     });
   },
+  toggleToDoUserCard: async (parent, args, { passport, prisma }, info) => {
+    const userId = passport.getUserId();
+    const { cardId } = args.data;
+
+    const queryArgs = {
+      where: {
+        id: cardId,
+        user: {
+          id: userId,
+        },
+      },
+    };
+
+    // Make sure that the card that is trying to be deleted belongs to the user
+    const userCard = await prisma.query.cards(queryArgs, '{ id isToDo }');
+
+    if (userCard.length > 0) {
+      const updateArgs = {
+        data: {
+          isToDo: !userCard[0].isToDo,
+        },
+        where: {
+          id: cardId,
+        },
+      };
+
+      const updatedCard = await prisma.mutation.updateCard(updateArgs, info);
+      return updatedCard;
+    }
+
+    throw new ServerError({
+      message: 'The user does not have access to delete this card',
+      status: 403,
+    });
+  },
 };
