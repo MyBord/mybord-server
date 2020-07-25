fe361d62bba295093d02f81f98108889ee80bf44
Jimmy Emery <jemery@allcampus.com>
2020-07-25 14:05:10 -0400

    udpating express session options

--- src/middleware/expressSessionOptions.ts
+++ src/middleware/expressSessionOptions.ts
@@ -1,9 +1,12 @@
 import uuid from 'uuid/v4';
+import { SessionOptions } from 'express-session';
 
-export default {
+const sessionOptions: SessionOptions = {
   cookie: { sameSite: 'strict', secure: true }, // cookie must be sent via https
   genid: (request) => uuid(), // generates a session ID
   resave: false,
   saveUninitialized: false,
   secret: process.env.SESSION_SECRET, // secret that is needed to sign the cookie
 };
+
+export default sessionOptions;
