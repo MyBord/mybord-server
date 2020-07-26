17394599e56ae54e21e8aeb98c0ee7e46eccccc9
Jimmy Emery <jemery@allcampus.com>
2020-07-26 10:08:52 -0400

    updating cors

--- src/middleware/corsOptions.ts
+++ src/middleware/corsOptions.ts
@@ -4,10 +4,11 @@ import { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
   credentials: true,
+  origin: 'http://localhost:8080',
 };
 
-if (process.env.NODE_ENV === 'DEV') {
-  corsOptions.origin = `http://localhost:${process.env.EXTERNAL_PORT}`;
-}
+// if (process.env.NODE_ENV === 'DEV') {
+//   corsOptions.origin = `http://localhost:${process.env.EXTERNAL_PORT}`;
+// }
 
 export default corsOptions;
