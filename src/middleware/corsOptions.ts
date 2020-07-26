a8b13c36d91d6ae4e3f79304755aeb74b50252db
Jimmy Emery <jemery@allcampus.com>
2020-07-25 20:24:03 -0400

    updating cors options

--- src/middleware/corsOptions.ts
+++ src/middleware/corsOptions.ts
@@ -1,4 +1,5 @@
 export default {
   credentials: true,
-  origin: `http://localhost:${process.env.EXTERNAL_PORT}`,
+  // origin: `http://localhost:${process.env.EXTERNAL_PORT}`,
+  origin: 'https://mbh-server-dev-jimmy.herokuapp.com',
 };
