c32f1fe7e71553c12930bd9078bc3201a64791f7
Jimmy Emery <jemery@allcampus.com>
2020-07-26 11:45:52 -0400

    updating cors options

--- src/middleware/corsOptions.ts
+++ src/middleware/corsOptions.ts
@@ -7,20 +7,18 @@ const corsOptions: CorsOptions = {
   // origin: 'http://localhost:8080',
 };
 
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
-console.log('----------- DEBUGGING -------------');
 
 if (process.env.NODE_ENV === 'DEV') {
   // corsOptions.origin = `http://localhost:${process.env.EXTERNAL_PORT}`;
   corsOptions.origin = 'http://localhost:8080';
 }
+
+console.log('----------- DEBUGGING CORS -------------');
+console.log('CORS OPTIONS:');
+console.log(corsOptions);
+console.log('NODE ENV:');
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV === 'DEV');
+console.log('----------- DEBUGGING CORS -------------');
 
 export default corsOptions;
