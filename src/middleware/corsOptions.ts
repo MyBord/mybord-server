336b64fd4d62228cbe6784ca10de7651e8921169
Jimmy Emery <jemery@allcampus.com>
2020-07-26 11:32:39 -0400

    udpating cors

--- src/middleware/corsOptions.ts
+++ src/middleware/corsOptions.ts
@@ -4,11 +4,12 @@ import { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
   credentials: true,
-  origin: 'http://localhost:8080',
+  // origin: 'http://localhost:8080',
 };
 
-// if (process.env.NODE_ENV === 'DEV') {
-//   corsOptions.origin = `http://localhost:${process.env.EXTERNAL_PORT}`;
-// }
+if (process.env.NODE_ENV === 'DEV') {
+  // corsOptions.origin = `http://localhost:${process.env.EXTERNAL_PORT}`;
+  corsOptions.origin = 'http://localhost:8080';
+}
 
 export default corsOptions;
