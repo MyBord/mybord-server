d704fd67fd105ae0237a92d6a8e281d3add23d92
Jimmy Emery <jemery@allcampus.com>
2020-08-08 18:40:43 -0400

    updating

--- src/foo.ts
+++ src/foo.ts
@@ -1,19 +1,57 @@
+// Sources:
+// * https://www.nodegit.org/guides/cloning/ssh-with-agent/
+// * https://radek.io/2015/10/27/nodegit/
+
 // todo: change to import statement
 const nodegit = require('nodegit');
+const path = require('path');
+// remove fs
+const fs = require('fs');
 
-// ----- SET CLONE URL ----- //
+// ----- 1. SET CLONE URL ----- //
 
 const cloneUrl = 'https://github.com/jimmy-e/mybord';
 
-// ----- SET FOLDER THAT STORES CLONED REPOSITORY ----- //
+// ----- 2. SET FOLDER THAT STORES CLONED REPOSITORY ----- //
 
 // todo: folder name should be 'client'
-// const path = 'client';
+// const localPath = path.join(__dirname, 'client');
 
 const d = new Date();
-const path = `client-${d.getHours}${d.getMinutes()}`;
+const localPath = `client-${d.getHours}${d.getMinutes()}`;
 
-// ----- SET CLONE OPTIONS ----- //
+// ----- 3. SET CLONE OPTIONS ----- //
+
+const sshFolder = path.join(__dirname, '../../../', '.ssh/');
+const id_rsa_file = path.join(sshFolder, 'id_rsa');
+const id_rsa_pub_file = path.join(sshFolder, 'id_rsa.pub');
 
 
-nodegit.Clone(cloneUrl, path);
+const cloneOptions = {
+  fetchOpts: {
+    callbacks: {
+      // change fn notation
+      certificateCheck() { return 0; },
+      credentials(url, userName) {
+        return nodegit.Cred.sshKeyNew(
+          userName,
+          id_rsa_pub_file,
+          id_rsa_file,
+          '<your-passphrase-here>',
+        );
+      },
+    },
+  },
+};
+
+// ----- 4. CLONE THE REPOSITORY ----- //
+
+const cloneClientRepository = async (): Promise<void> => {
+  try {
+    await nodegit.Clone(cloneUrl, localPath, cloneOptions);
+  } catch (error) {
+    throw Error(error);
+  }
+};
+
+cloneClientRepository();
