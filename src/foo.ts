9723edded9e4c8cbd91f86256160d874d7b724a0
Jimmy Emery <jemery@allcampus.com>
2020-08-09 11:08:56 -0400

    updating

--- src/foo.ts
+++ src/foo.ts
@@ -8,51 +8,55 @@ const path = require('path');
 // remove fs
 const fs = require('fs');
 
-// ----- 1. SET CLONE URL ----- //
+const foopath = path.join(__dirname, 'client');
+console.log(foopath);
+// fs.rmdirSync(dir, { recursive: true });
 
-const cloneUrl = 'git@github.com:jimmy-e/mybord.git';
-
-// ----- 2. SET FOLDER THAT STORES CLONED REPOSITORY ----- //
-
-// todo: folder name should be 'client'
-// const localPath = path.join(__dirname, 'client');
-
-const d = new Date();
-const localPath = `client-${d.getHours()}${d.getMinutes()}${d.getSeconds()}`;
-
-// ----- 3. SET CLONE OPTIONS ----- //
-
-const sshFolder = path.join(__dirname, '../../../', '.ssh/');
-const id_rsa_file = path.join(sshFolder, 'id_rsa');
-const id_rsa_pub_file = path.join(sshFolder, 'id_rsa.pub');
-
-const certificateCheck = (): number => 0;
-
-const credentials = (url, username): object => nodegit.Cred.sshKeyNew(
-  username,
-  id_rsa_pub_file,
-  id_rsa_file,
-  'foo', // it does not seem to care what the passphrase is
-);
-
-const cloneOptions = {
-  fetchOpts: {
-    callbacks: {
-      certificateCheck,
-      credentials,
-    },
-  },
-};
-
-// ----- 4. CLONE THE REPOSITORY ----- //
-
-const cloneRepository = async (): Promise<void> => {
-  try {
-    const clonedRepository = await nodegit.Clone(cloneUrl, localPath, cloneOptions);
-  } catch (error) {
-    console.log('error: could not clone');
-    console.log(error);
-  }
-};
-
-cloneRepository();
+// // ----- 1. SET CLONE URL ----- //
+//
+// const cloneUrl = 'git@github.com:jimmy-e/mybord.git';
+//
+// // ----- 2. SET FOLDER THAT STORES CLONED REPOSITORY ----- //
+//
+// // todo: folder name should be 'client'
+// // const localpath = path.join(__dirname, 'client');
+//
+// const d = new Date();
+// const localPath = `client-${d.getHours()}${d.getMinutes()}${d.getSeconds()}`;
+//
+// // ----- 3. SET CLONE OPTIONS ----- //
+//
+// const sshFolder = path.join(__dirname, '../../../', '.ssh/');
+// const id_rsa_file = path.join(sshFolder, 'id_rsa');
+// const id_rsa_pub_file = path.join(sshFolder, 'id_rsa.pub');
+//
+// const certificateCheck = (): number => 0;
+//
+// const credentials = (url, username): object => nodegit.Cred.sshKeyNew(
+//   username,
+//   id_rsa_pub_file,
+//   id_rsa_file,
+//   'foo', // it does not seem to care what the passphrase is
+// );
+//
+// const cloneOptions = {
+//   fetchOpts: {
+//     callbacks: {
+//       certificateCheck,
+//       credentials,
+//     },
+//   },
+// };
+//
+// // ----- 4. CLONE THE REPOSITORY ----- //
+//
+// const cloneRepository = async (): Promise<void> => {
+//   try {
+//     const clonedRepository = await nodegit.Clone(cloneUrl, localPath, cloneOptions);
+//   } catch (error) {
+//     console.log('error: could not clone');
+//     console.log(error);
+//   }
+// };
+//
+// cloneRepository();
