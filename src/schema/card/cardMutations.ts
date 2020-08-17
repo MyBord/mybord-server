d1db7469f32b52f4f7e4dfc8367eed7289dcfe73
Jimmy Emery <jemery@allcampus.com>
2020-08-17 15:36:29 -0400

    updating

--- src/schema/card/cardMutations.ts
+++ src/schema/card/cardMutations.ts
@@ -80,8 +80,8 @@ export default {
       const youtubeVideoData = await youtube.getYoutubeVideoData(videoId);
 
       return {
-        cardType: cardEnums.youtube,
         title: youtubeVideoData.channelTitle,
+        type: cardEnums.youtube,
         youtubeCardData: youtubeVideoData,
       };
     } catch (error) {
