19e131857d4eaa47c5c88a7f7662ce52f685a3fd
Jimmy Emery <jimmy.e@outlook.com>
2020-09-13 17:11:06 -0400

    updating card utils

--- src/schema/card/cardUtils/cardEnums.ts
+++ src/schema/card/cardUtils/cardEnums.ts
@@ -1,4 +1,9 @@
-export default {
+import { CardCategory, CardType } from './cardTypes';
+
+export const category: Record<string, CardCategory> = {
   video: 'Video',
+};
+
+export const type: Record<string, CardType> = {
   youtube: 'Youtube',
 };
--- src/schema/card/cardUtils/cardUtils.ts
+++ src/schema/card/cardUtils/cardUtils.ts
@@ -1,12 +1,43 @@
-import { CardType } from './cardTypes';
+import youtube from 'youtube/youtube';
+import { category } from 'schema/card/cardUtils/cardEnums';
+import { CardType, InitialCardDataSchema } from './cardTypes';
 
-export const getCardType = (url: string): CardType => {
+const getCardType = (url: string): CardType => {
   if (
     url.includes('youtube.com')
     || url.includes('youtu.be')
     || url.includes('youtube-nocookie.com')
   ) {
     return 'Youtube';
+  }
+
+  if (url.endsWith('.jpg') || url.endsWith('.png')) {
+    return 'Image';
+  }
+
+  throw Error('Cannot detect a valid card type');
+};
+
+
+const getInitialYoutubeData = async (url: string): Promise<InitialCardDataSchema> => {
+  const youtubeVideoData = await youtube.getYoutubeVideoData(url);
+
+  return {
+    cardData: {
+      youtubeCardData: youtubeVideoData,
+    },
+    category: category.video,
+    title: youtubeVideoData.videoTitle,
+    url,
+  };
+};
+
+export const getInitialCardData = async (url: string): Promise<InitialCardDataSchema> => {
+  const cardType = getCardType(url);
+
+  if (cardType === 'Youtube') {
+    const initialYoutubeData = await getInitialYoutubeData(url);
+    return initialYoutubeData;
   }
 
   throw Error('Cannot detect a valid card type');
