8079b6b143a37b335489a684dcfa5c9aa404b101
Jimmy Emery <jimmy.e@outlook.com>
2020-10-11 14:13:23 -0400

    updating error throwing

--- src/schema/user/userResolvers/userMutations/createUser.ts
+++ src/schema/user/userResolvers/userMutations/createUser.ts
@@ -20,7 +20,7 @@ export default async (parent, args, { passport, prisma }, info) => {
     return restrictUserData(user);
   } catch (error) {
     if (error.message === 'invalid username') {
-      throw new ServerError({ message: 'invalid username', status: 400 });
+      throw new ServerError({ message: error.message, status: 400 });
     }
     if (error.message.includes('unique constraint')) {
       throw new ServerError({ message: 'duplicate user', status: 400 });
