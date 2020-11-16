c6e10ddf2ee36d6d24287aedb12af5ad282682fd
Jimmy Emery <jimmy.e@outlook.com>
2020-11-16 15:28:57 -0500

    updating playground settings

--- src/server/serverPlaygroundSettings.ts
+++ src/server/serverPlaygroundSettings.ts
@@ -1,9 +1,9 @@
 import { PlaygroundConfig } from 'apollo-server-express';
 
 const getServerPlaygroundSettings = (): PlaygroundConfig => {
-  if (process.env.MODE === 'PROD') {
-    return false;
-  }
+  // if (process.env.MODE === 'PROD') {
+  //   return false;
+  // }
 
   return {
     settings: {
